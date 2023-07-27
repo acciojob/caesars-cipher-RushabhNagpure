9c61d2c7b2fafea4b77e721902da9bccd4175aa0
RushabhNagpure <114604410+RushabhNagpure@users.noreply.github.com>
2023-07-27 12:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,15 +32,34 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+	const A = 'A'.charCodeAt();
+	const Z = 'Z'.charCodeAt();
+	const shift = 13;
+	
+  let decodedArr = [];// Your Result goes here
+	for (let index = 0; index < encodedStr.length; index++) {
+		const char = encodedStr[i];
+		const charCode = char.charCodeAt();
+
+		if (charCode >= A && charCode <= Z) {
+			let decodedCharCode = charCode - shift;
+			if (decodedCharCode < A) {
+				decodedCharCode += 26;
+			}
+			decodedArr += String.fromCharCode(decodedCharCode);
+		}	else {
+				decodedArr += char;
+			}
+		}
+	
   // Only change code below this line
 
-  return; //return decodedArr
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
